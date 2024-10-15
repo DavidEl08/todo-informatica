36666c96f500d1463012c096c3fd91e3f7544471
DavidEl08 <dadinut20@iesrioarba.es>
2024-10-15 10:13:59 +0200

    Update david.intento.py

--- david.intento.py
+++ david.intento.py
@@ -1,14 +1,11 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-
 def sum(a, b):
     return (a + b)
 
 a = int(input('cual es el primer numero: '))
 b = int(input('añade otro numerin: '))
 
-if (a == '7') and (b == '770'):
+if (a == 7) and (b == 770):
     print('eyyy muy buenas a todos guapisimos')
 
-if (a != '7') and (b != '770'):
+else:
     print(f'Sum of {a} and {b} is {sum(a, b)}')
