66dd224945d41f6329bc1b05fb59d54ada364451
DavidEl08 <dadinut20@iesrioarba.es>
2024-10-16 13:32:39 +0200

    Update david.if-else.py

--- david.if-else.py
+++ david.if-else.py
@@ -5,7 +5,7 @@ a = int(input('cual es el primer numero: '))
 b = int(input('añade otro numerin: '))
 
 if ((a + b)== 777):
-    print('eyyy muy buenas a todos guapisimos')
-
+    print('eyyy muy buenas a todos guapisimos aqui VEGETTA 777')
+    print(f'Sum of {a} and {b} is {sum(a, b)}')
 else:
     print(f'Sum of {a} and {b} is {sum(a, b)}')
